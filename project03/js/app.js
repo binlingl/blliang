2d8858f1fbb7c54297bcce56c3e07ec0d10714dd
muye <627483835@qq.com>
2017-05-16 23:00:44 +0800

    test

--- project03/js/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by admin on 2017/5/8.
- */
-define(["angular","ui-router"],function(angular){
-    var myapp = angular.module("myApp",["ui.router"]);
-    return myapp;
-});
--- project03/js/ctrl/use.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by admin on 2017/5/4.
- */
-define("one",function(angular){
-    var myModule = angular.controller("useThis",["one",function(one){
-
-    }])
-})
